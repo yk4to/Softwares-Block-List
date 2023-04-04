2df89482e475ec7f7ba456493a5b8d5e1c9d63dc
fus1ondev <luke_s1220@yahoo.co.jp>
2023-04-04 13:35:21 +0900

    Add some websites

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const text = fs.readFileSync("blocklist.txt", "utf8");
-const websites = text.split("\n");
+const websites = [...new Set(text.split("\n"))];
 const sorted = websites.sort();
 console.log("Sorted");
 fs.writeFileSync("blocklist.txt", sorted.join("\n"));
